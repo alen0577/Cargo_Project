cfb7c96283102ae923ed1a409d287058809b4c1e
Alen Antony <alenantony32@gmail.com>
2024-04-24 22:48:50 +0530

    50th commit

--- Cargo_Team/urls.py
+++ Cargo_Team/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('team-dashboard/',views.team_dashboard,name='team_dashboard'),    
+    path('team-dashboard/',views.team_dashboard,name='team_dashboard'),  
+    path('team-profile/',views.team_profile,name='team_profile'),  
 ]
--- Cargo_Team/views.py
+++ Cargo_Team/views.py
@@ -23,3 +23,19 @@ def team_dashboard(request):
         return render(request, 'team_dashboard.html', context)
     else:
         return redirect('/')
+
+# team profile page
+def team_profile(request):
+    if 'login_id' in request.session:
+        log_id = request.session['login_id']
+        if 'login_id' not in request.session:
+            return redirect('/')
+        
+        dash_details = CargoTeam.objects.get(id=log_id,admin_approval=1,is_active=1)
+        
+        context = {
+            'details': dash_details,
+        }
+        return render(request, 'team_profile.html', context)
+    else:
+        return redirect('/')
